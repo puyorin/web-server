435889462431774c7afb7017b2c3e40e7e859a5c
puyorin <fuka1664@gmail.com>
2023-10-12 21:49:14 +0900

    First commit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.static(path.join(__dirname, "public")))
 //   console.log(req)
 //   res.send("<h1>トップページ!!</h1>")
 // })
+
 app.post("/api/v1/quiz", function (req, res) {
     const answer = req.body.answer;
     if(answer === "2"){
